571f394c8f3923b75463f0fe06c3eaff0832e08f
Nevil George <nevilgeorge2016@u.northwestern.edu>
2014-09-03 01:07:39 -0500

    twitter API credentials added

--- app.js
+++ app.js
@@ -8,13 +8,27 @@ var express = require('express'),
 	spotify = require('spotify-web'),
 	socketio = require('socket.io'),
 	http = require('http'),
-	twitter = require('twit');
+	Twit = require('twit');
 
 // Instantiate the app. Sort of weird because we are instantiating an Express/Socket.io app
 var app = express(),
 	server = http.Server(app),
 	io = socketio(server);
 
-server.listen(8080, function() {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(morgan('dev'));
+app.use(cookieParser);
+
+var twitter = new Twit({
+		consumer_key: 'Bgrn71RmP4qNjSXqOpCgdqTN7',
+		consumer_secret: 'YIt8tFn2bC6UvxFC0DuBJyFepPVCe6XMFWiprougq0OD5TDsnm',
+		access_token: '2787350504-LEaKQvlst18hjFkOMavwqmg85BoRgU0aP6aUFzR',
+		access_token_secret: 'RRUO9wIsOLReZTlngsF2WEwVAYsDlB6EGTwRdKt6tGFpq'
+});
+
+
+// listen on port 8888
+server.listen(8888, function() {
 		console.log('App listening on port 8080...');
 });	
