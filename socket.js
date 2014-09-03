c1d885604f358d9629b6e7a3af6ab4662d17d08a
Nevil George <nevilgeorge2016@u.northwestern.edu>
2014-09-03 12:55:06 -0500

    tweets parsed to create array of keywords

--- socket.js
+++ socket.js
@@ -2,8 +2,14 @@
 
 module.exports = function(io, twitter) {
 	var tweetParser = function(string) {
-
-	}
+		var wordArray = string.split(' ');
+		// Iterate through wordArray and remove username and dash
+		for (var i = 0; i < wordArray.length; i++) {
+			if (wordArray[i][0] === '@' || wordArray[i] === '-') {
+				wordArray.splice(i, 1);
+			}
+		}
+	};
 	var stream = twitter.stream('statuses/filter', { track: '@__nevil' });
 	io.on('connection', function(socket) {
 		console.log('socket.io connection established!');
