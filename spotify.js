801be76d61ac56d127e256b2c03a7889e11d23fb
Nevil George <nevilgeorge2016@u.northwestern.edu>
2014-09-03 19:29:45 -0500

    adding gitignore file and removing credentials

--- spotify.js
+++ spotify.js
@@ -2,13 +2,13 @@
 
 var Spotify = require('spotify-web'),
 	lame = require('lame'),
-	Speaker = require('speaker');
+	Speaker = require('speaker'),
+	login = require('./loginCredentials.js');
 
 module.exports = function(keywordArray) {
-	var username = 'nevilgeorge',
-		password = '__nevil__',
+	var username = login.spotify.username,
+		password = login.spotify.password,
 		uri = 'spotify:track:6tdp8sdXrXlPV6AZZN2PE8';
-		console.log(Spotify);
 
 		Spotify.login(username, password, function(err, spotify) {
 			if (err) {
