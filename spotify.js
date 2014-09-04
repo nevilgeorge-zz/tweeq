135d8b2fb88647eb1279099c06feba774b19ed1d
Nevil George <nevilgeorge2016@u.northwestern.edu>
2014-09-04 13:49:30 -0500

    searching and playing working

--- spotify.js
+++ spotify.js
@@ -4,50 +4,12 @@ var Spotify = require('spotify-web'),
 	lame = require('lame'),
 	Speaker = require('speaker'),
 	xml2js = require('xml2js'),
+	async = require('async'),
 	login = require('./loginCredentials.js');
 
-var searchForTrack = function(spotify, query) {
-	spotify.search(query, function(err, xml) {
-		if (err) {
-			throw err;
-		}
-
-		var parser = new xml2js.Parser();
-		parser.parseString(xml);
-		parser.on('end', function(data) {
-			var returnObj, trackID;
-			returnObj = JSON.stringify(data, null, 2);
-			console.log(returnObj);
-			var trackID = returnObj.result.tracks[0].track.id;
-			return trackID;
-		});
-
-	});
-},
-	convertToURI = function(trackID) {
-		return Spotify.id2uri('track', trackID);
-	},
-
-	playTrack = function(spotify, uri) {
-		spotify.get(uri, function(err, track) {
-			if (err) {
-				throw err;
-			}
-			console.log('Playing: %s - %s', track.artist[0].name, track.name);
-
-			track.play()
-				.pipe(new lame.Decoder())
-				.pipe(new Speaker())
-				.on('finish', function() {
-					console.log('Track finished.');
-				});
-		});
-	}
-
-
-
 module.exports = function(query) {
-	var username = login.spotify.username,
+	var trackID, uri,
+		username = login.spotify.username,
 		password = login.spotify.password,
 		//uri = 'spotify:track:6tdp8sdXrXlPV6AZZN2PE8',
 		query = 'work out j cole';
@@ -56,12 +18,80 @@ module.exports = function(query) {
 			if (err) {
 				throw err;
 			}
-
-			var trackID = searchForTrack(spotify, query);
 			
-			uri = convertToURI(trackID);
+			spotify.search(query, function(err, xml) {
+				if (err) {
+					throw err;
+				}
+				console.log('hi');
+				var parser = new xml2js.Parser();
+				parser.on('end', function(data) {
+					//console.log(JSON.stringify(data, null, 2).result.tracks[0].track);
+					console.log(data.result.tracks[0].track[0].id[0]);
+					trackID = data.result.tracks[0].track[0].id[0];
+				});
+				parser.parseString(xml);
 
-			playTrack(spotify, trackID);
-			
+			});
+
+			setTimeout(function() {
+				uri = Spotify.id2uri('track', trackID);
+				console.log(uri);
+			}, 1000);
+
+			setTimeout(function() {
+				spotify.get(uri, function(err, track) {
+					if (err) {
+						throw err;
+					}
+					console.log('Playing: %s - %s', track.artist[0].name, track.name);
+
+					track.play()
+					.pipe(new lame.Decoder())
+					.pipe(new Speaker())
+					.on('finish', function() {
+						console.log('Track finished.');
+					});
+				});
+			}, 1500);
+
 		});
-}
+};
+
+// var searchForTrack = function(spotify, query) {
+// 	spotify.search(query, function(err, xml) {
+// 		if (err) {
+// 			throw err;
+// 		}
+// 		console.log('hi');
+// 		var parser = new xml2js.Parser();
+// 		parser.parseString(xml);
+// 		parser.on('end', function(data) {
+// 			var returnObj, trackID;
+// 			returnObj = JSON.stringify(data, null, 2);
+// 			trackID = returnObj.result.tracks[0].track.id;
+// 			console.log(trackID);
+// 			return trackID;
+// 		});
+
+// 	});
+// };
+// var convertToURI = function(trackID) {
+// 		return Spotify.id2uri('track', trackID);
+// 	};
+
+// var playTrack = function(spotify, uri) {
+// 		spotify.get(uri, function(err, track) {
+// 			if (err) {
+// 				throw err;
+// 			}
+// 			console.log('Playing: %s - %s', track.artist[0].name, track.name);
+
+// 			track.play()
+// 				.pipe(new lame.Decoder())
+// 				.pipe(new Speaker())
+// 				.on('finish', function() {
+// 					console.log('Track finished.');
+// 				});
+// 		});
+// 	};
