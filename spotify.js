fd9c702cdfd3c2f577e7dc3bc3d8f01b394b49cb
Nevil George <nevilgeorge2016@u.northwestern.edu>
2014-09-04 22:55:38 -0500

    music playing, uses setTimeout

--- spotify.js
+++ spotify.js
@@ -26,8 +26,6 @@ module.exports = function(query) {
 				console.log('hi');
 				var parser = new xml2js.Parser();
 				parser.on('end', function(data) {
-					//console.log(JSON.stringify(data, null, 2).result.tracks[0].track);
-					console.log(data.result.tracks[0].track[0].id[0]);
 					trackID = data.result.tracks[0].track[0].id[0];
 				});
 				parser.parseString(xml);
@@ -36,8 +34,7 @@ module.exports = function(query) {
 
 			setTimeout(function() {
 				uri = Spotify.id2uri('track', trackID);
-				console.log(uri);
-			}, 1000);
+			}, 1500);
 
 			setTimeout(function() {
 				spotify.get(uri, function(err, track) {
@@ -53,45 +50,7 @@ module.exports = function(query) {
 						console.log('Track finished.');
 					});
 				});
-			}, 1500);
+			}, 2000);
 
 		});
 };
-
-// var searchForTrack = function(spotify, query) {
-// 	spotify.search(query, function(err, xml) {
-// 		if (err) {
-// 			throw err;
-// 		}
-// 		console.log('hi');
-// 		var parser = new xml2js.Parser();
-// 		parser.parseString(xml);
-// 		parser.on('end', function(data) {
-// 			var returnObj, trackID;
-// 			returnObj = JSON.stringify(data, null, 2);
-// 			trackID = returnObj.result.tracks[0].track.id;
-// 			console.log(trackID);
-// 			return trackID;
-// 		});
-
-// 	});
-// };
-// var convertToURI = function(trackID) {
-// 		return Spotify.id2uri('track', trackID);
-// 	};
-
-// var playTrack = function(spotify, uri) {
-// 		spotify.get(uri, function(err, track) {
-// 			if (err) {
-// 				throw err;
-// 			}
-// 			console.log('Playing: %s - %s', track.artist[0].name, track.name);
-
-// 			track.play()
-// 				.pipe(new lame.Decoder())
-// 				.pipe(new Speaker())
-// 				.on('finish', function() {
-// 					console.log('Track finished.');
-// 				});
-// 		});
-// 	};
