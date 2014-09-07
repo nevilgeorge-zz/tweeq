81ca299d08eae01963fb81ffc025a1e43db087d8
Nevil George <nevilgeorge2016@u.northwestern.edu>
2014-09-06 23:50:20 -0500

    commenting files

--- spotify.js
+++ spotify.js
@@ -1,4 +1,4 @@
-// spotify.js
+// spotify.js - takes care of Spotify Web API commands
 
 var Spotify = require('spotify-web'),
 	lame = require('lame'),
@@ -9,8 +9,44 @@ var Spotify = require('spotify-web'),
 	login = require('./loginCredentials.js');
 
 var trackPlaying = false,
-	i = 0,
+	// Queue to store songs that are requested while another song is still playing
 	playQueue = [];
+
+
+// Implementing an enqueue function for the queue of songs
+var enqueue = function(playQueue, uri) {
+	playQueue.push(uri);
+},
+
+// Implementing a dequeue function for the queue of songs
+dequeue = function(playQueue) {
+	return playQueue.shift();
+},
+
+// Plays a track that is passed into the function
+playTrack = function(track) {
+	console.log('Playing: %s - %s', track.artist[0].name, track.name);
+	trackPlaying = true;
+	track.play()
+	.pipe(new lame.Decoder())
+	.pipe(new Speaker())
+	.on('finish', function() {
+		trackPlaying = false;
+		nextTrack = dequeue(playQueue);
+		// check if there is another track to be played, if so, then play it.
+		/*
+		Similar to a tail recursion but not exactly because playTrack completes execution
+		before being called again.
+		*/
+		if (typeof nextTrack !== 'undefined') {
+			console.log('Getting next track...');
+			playTrack(nextTrack);
+		} else {
+			console.log('Done playing all songs on queue!');
+		}
+	});
+};
+
 
 module.exports = function(query) {
 	var trackID, uri, currentTrack, nextTrack,
@@ -37,10 +73,12 @@ module.exports = function(query) {
 
 			});
 
+			// Wait for search to return. Then convert the song ID to a Spotify URI
 			setTimeout(function() {
 				uri = Spotify.id2uri('track', trackID);
 			}, 1000);
 
+			// Once a URI is obtained, get the associated track and then pass it to the playTrack function
 			setTimeout(function() {
 				spotify.get(uri, function(err, track) {
 					if (err) {
@@ -48,7 +86,9 @@ module.exports = function(query) {
 					}
 
 					console.log('Found!');
+					// Check if there is currently a track playing and check if there is another track to be played
 					if (!trackPlaying && typeof track !== 'undefined') {
+						// Only plays the first song in the queue
 						playTrack(track);
 					} else {
 						enqueue(playQueue, track);
@@ -58,29 +98,4 @@ module.exports = function(query) {
 			}, 1500);
 
 		});
-};
-
-var enqueue = function(playQueue, uri) {
-	playQueue.push(uri);
-},
-dequeue = function(playQueue) {
-	return playQueue.shift();
-};
-
-var playTrack = function(track) {
-	console.log('Playing: %s - %s', track.artist[0].name, track.name);
-	trackPlaying = true;
-	track.play()
-	.pipe(new lame.Decoder())
-	.pipe(new Speaker())
-	.on('finish', function() {
-		trackPlaying = false;
-		nextTrack = dequeue(playQueue);
-		if (typeof nextTrack !== 'undefined') {
-			console.log('Getting next track...');
-			playTrack(nextTrack);
-		} else {
-			console.log('Done playing all songs on queue!');
-		}
-	});
 };
